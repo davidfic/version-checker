03ad518a1f0f8d257d3403cec456b964c94a63a7
David Ficociello <davidfic@gmail.com>
2019-12-20 17:28:17 -0500

    adding gke versions

--- app.py
+++ app.py
@@ -5,27 +5,49 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-def get_version(base_url):
-    resp = requests.get(base_url)
+hashicorp_base_url = 'https://releases.hashicorp.com/'
+
+release_channel = {'stable': 'https://cloud.google.com/kubernetes-engine/docs/release-notes-stable', 
+                    'regular': 'https://cloud.google.com/kubernetes-engine/docs/release-notes-regular',
+                    'rapid': 'https://cloud.google.com/kubernetes-engine/docs/release-notes-rapid'
+}
+
+def get_version(url):
+    resp = requests.get(url)
     soup = BeautifulSoup(resp.text, 'html.parser')
     release_list = soup.find_all('a')
     return str(release_list[1])[str(release_list[1]).index('>')+1:-4]
 
 @app.route('/terraform')
 def get_terraform_version():
-    # base_url = 'https://releases.hashicorp.com/terraform'
-
-    
-    # resp = requests.get(base_url)
-    # soup = BeautifulSoup(resp.text, 'html.parser')
-    # release_list = soup.find_all('a')
-    # return str(release_list[1])[str(release_list[1]).index('>')+1:-4]
-    return get_version('https://releases.hashicorp.com/terraform')
+    return jsonify(get_version(hashicorp_base_url + 'terraform'))
 
 @app.route('/vault')
 def get_vault_latest_version():
-    return get_version('https://releases.hashicorp.com/vault')
-    # base_url = 'https://releases.hashicorp.com/vault'
+    return jsonify(get_version(hashicorp_base_url + 'vault'))
+
+@app.route('/')
+def index():
+    return 'hello'
+
+
+def gke_release_version(channel):
+    resp = requests.get(channel)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    stable_release_number = soup.find('div', {'class': 'release-changed'}).find('p')
+    str_rel = str(stable_release_number)
+    rel_num = str_rel[3:str_rel.index(' ')]
+    return rel_num
+
+@app.route('/gke-stable')
+def get_gke_stable_release():
+    return jsonify(gke_release_version(release_channel['stable']))
+
+
+@app.route('/gke-regular')
+def get_gke_regular_release():
+    return jsonify(gke_release_version(release_channel['regular']))
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port='8080')
